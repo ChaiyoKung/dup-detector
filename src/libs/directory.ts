68eedc69b72e2f47ff156c040a7310e639c659ee
ChaiyoKung <kananek45@gmail.com>
2024-08-05 22:18:37 +0700

    fix: use absolute path instead for cache efficient

--- src/libs/directory.ts
+++ src/libs/directory.ts
@@ -1,13 +1,13 @@
 import { type PathLike } from "node:fs";
 import { readdir } from "node:fs/promises";
-import { join } from "node:path";
+import { resolve } from "node:path";
 
 export async function traverseDirectory(path: PathLike, ignoredDirs: string[] = []): Promise<string[]> {
   const fileList: string[] = [];
 
   const dirents = await readdir(path, { withFileTypes: true });
   for (const dirent of dirents) {
-    const fullPath = join(dirent.parentPath, dirent.name);
+    const fullPath = resolve(dirent.parentPath, dirent.name);
     if (dirent.isDirectory()) {
       if (ignoredDirs.includes(dirent.name)) continue;
       fileList.push(...(await traverseDirectory(fullPath, ignoredDirs)));
