192c6068b2c94e5d80cdd85a976ae4bf404bf3cc
ChaiyoKung <kananek45@gmail.com>
2024-08-07 20:52:30 +0700

    refactor: extract into `getFileHash` function

--- src/find-duplicate-files.ts
+++ src/find-duplicate-files.ts
@@ -1,22 +1,14 @@
 import { type PathLike } from "node:fs";
 import { setToArray } from "./utils/set";
 import { traverseDirectory } from "./libs/directory";
-import { calculateFileHash, getCacheKeyFromPath } from "./libs/file";
-import { Cache } from "file-system-cache";
+import { getFileHash } from "./libs/file";
 
 export async function findDuplicateFiles(dirPath: PathLike, ignoredDirs: string[] = []) {
   const fileHashes = new Map<string, string[]>();
   const duplicates = new Set<string[]>();
-  const cache = new Cache({ basePath: "./.dup-detector/.cache" });
 
   for await (const filePath of traverseDirectory(dirPath, ignoredDirs)) {
-    const cacheKey = await getCacheKeyFromPath(filePath);
-    let fileHash = await cache.get(cacheKey);
-    if (fileHash === undefined) {
-      fileHash = await calculateFileHash(filePath);
-      await cache.set(cacheKey, fileHash);
-    }
-
+    const fileHash = await getFileHash(filePath);
     const existingFiles = fileHashes.get(fileHash);
     if (existingFiles === undefined) {
       fileHashes.set(fileHash, [filePath]);
--- src/libs/file.ts
+++ src/libs/file.ts
@@ -1,6 +1,10 @@
 import { createReadStream, type PathLike } from "node:fs";
 import { stat } from "node:fs/promises";
 import { createHash } from "node:crypto";
+import { Cache } from "file-system-cache";
+import { resolve } from "node:path";
+
+export const fileHashCache = new Cache({ basePath: resolve(__dirname, "../../.dup-detector/.cache") });
 
 export function calculateFileHash(path: PathLike) {
   return new Promise<string>((resolve, reject) => {
@@ -15,4 +19,14 @@ export function calculateFileHash(path: PathLike) {
 export async function getCacheKeyFromPath(path: PathLike) {
   const fileStat = await stat(path);
   return [path, fileStat.mtimeMs].join(":");
+}
+
+export async function getFileHash(filePath: string) {
+  const cacheKey = await getCacheKeyFromPath(filePath);
+  let fileHash = await fileHashCache.get(cacheKey);
+  if (fileHash === undefined) {
+    fileHash = await calculateFileHash(filePath);
+    await fileHashCache.set(cacheKey, fileHash);
+  }
+  return fileHash;
 }
