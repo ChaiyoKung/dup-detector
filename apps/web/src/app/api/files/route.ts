1ff593d86edc61b3227e511cf89e5b11281426cf
ChaiyoKung <kananek45@gmail.com>
2024-08-12 09:40:05 +0700

    feat(web): add response file endpoint

---
+++ apps/web/src/app/api/files/route.ts
@@ -0,0 +1,31 @@
+import { contentType } from "mime-types";
+import { NextRequest, NextResponse } from "next/server";
+import { existsSync } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { extractErrorMessage } from "../_utils/extract-error-message";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const path = searchParams.get("path");
+  if (!path) {
+    return NextResponse.json({ message: "file not found" }, { status: 404 });
+  }
+
+  if (!existsSync(path)) {
+    return NextResponse.json({ message: "file not found" }, { status: 404 });
+  }
+
+  const type = contentType(path);
+  if (!type) {
+    return NextResponse.json({ message: "invalid file type" }, { status: 400 });
+  }
+
+  try {
+    const data = await readFile(path);
+    return new NextResponse(data, { headers: { "Content-Type": type } });
+  } catch (error) {
+    console.error(error);
+    const errorMessage = extractErrorMessage(error);
+    return NextResponse.json({ message: errorMessage }, { status: 500 });
+  }
+}
