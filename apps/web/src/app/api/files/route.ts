fbbf56a02f5ce34f83ad9994f51dfb8f8522d99a
ChaiyoKung <kananek45@gmail.com>
2024-08-18 15:33:03 +0700

    feat(web): add delete file endpoint

--- apps/web/src/app/api/files/route.ts
+++ apps/web/src/app/api/files/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { existsSync } from "node:fs";
 import { readFile } from "node:fs/promises";
 import { extractErrorMessage } from "../../../../utils/extract-error-message";
+import { deleteFile } from "@dup-detector/modules";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
@@ -25,6 +26,22 @@ export async function GET(request: NextRequest) {
     return new NextResponse(data, { headers: { "Content-Type": type } });
   } catch (error) {
     console.error(error);
+    const errorMessage = extractErrorMessage(error);
+    return NextResponse.json({ message: errorMessage }, { status: 500 });
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const path = body.path;
+    if (!path) {
+      return NextResponse.json({ message: "file not found" }, { status: 404 });
+    }
+
+    await deleteFile(decodeURIComponent(path));
+    return Response.json({ message: "delete file success" });
+  } catch (error) {
     const errorMessage = extractErrorMessage(error);
     return NextResponse.json({ message: errorMessage }, { status: 500 });
   }
